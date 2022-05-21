c21a0b384b839958c809ca47b07c08144f10ae80
alonsogomezo <luisalonsogo95@gmail.com>
2022-05-21 04:52:44 +0000

    todo a medias

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,25 +1,52 @@
-import React from "react";
+import React, { useState } from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
-//create your first component
 const Home = () => {
+	const [newItem, setNewItem] = useState("");
+	const [items, setItems] = useState([]);
+	function addItem() {
+		if (!newItem) {
+			alert("Enter an item");
+			return;
+		}
+		const item = {
+			id: Math.floor(Math.random() * 1000),
+			value: newItem, //we get this from the input
+		};
+
+		setItems((oldList) => [...oldList, item]);
+		setNewItem("");
+		console.log(items);
+	}
+
+	function deleteItem(id) {
+		const newArray = items.filter((item) => item.id !== id);
+		setItems(newArray);
+	}
+
 	return (
-		<div>
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+		<center>
+			<h1>Todo</h1>
+			<input
+				type="text"
+				placeholder="Add an item..."
+				value={newItem}
+				onChange={(e) => setNewItem(e.target.value)}
+			/>
+			<button onClick={() => addItem()}>Add</button>
+			<ul>
+				{items.map((item) => {
+					return (
+						<li key={item.id}>
+							{item.value}{" "}
+							<button onClick={() => deleteItem(item.id)}>
+								{" "}
+								X{" "}
+							</button>
+						</li>
+					);
+				})}
+			</ul>
+		</center>
 	);
 };
 
